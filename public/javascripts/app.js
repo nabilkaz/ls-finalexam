fe1d0214245bca75ef1d3258925659c97a61b94e
Nabil Kazerouni <nabilkaz@gmail.com>
2020-09-22 17:00:41 -0700

    Form and Search

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -17,6 +17,18 @@ $(function(){
         Handlebars.registerPartial($partial.attr('id'), $partial.html());
     });
 
+    // template helper
+    function loadListView(){
+        contactList.list().then(function(data){
+            let context = {
+                "hasItems": Object.keys(data).length > 0,
+                "items": data
+            };
+    
+            $('main').html(templates.main_view(context));
+        });
+    }
+
     // eventhandlers-page
     // add contact
     $("main").on('click', 'a.eventhandler-add-contact', function(e){
@@ -25,21 +37,47 @@ $(function(){
         $('main').html(templates.form_template);
     });
 
+    //search
+    $("main").on('keyup', 'input.eventhandler-search-list', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        // replace list with filtered one based on string
+        contactList.list().then(function(data){
+            let term = $(e.target).val();
+            let filteredContacts = data.filter(function(contact){
+                return contact.full_name.includes(term);
+            });
+
+            let context = {
+                "hasItems": Object.keys(filteredContacts).length > 0,
+                "term": term,
+                "items": filteredContacts
+            };
+            $('#list').replaceWith(templates.list_view(context));
+        });
+    });
+    
     //eventhandlers-form
     // cancel
     $("main").on('click', '#form-cancel', function(e){
         e.stopPropagation();
         e.preventDefault();
-        $('main').html(templates.list_view);
+        loadListView();
     });
     
     // submit
     $("main").on('click', 'button[type=submit]', function(e){
         e.stopPropagation();
         e.preventDefault();
-        let data = objectifyFormArray($('form').serializeArray());
-        contactList.add(data);
-        contactList.list().then((data)=>console.table(data));
+        let $form = $('form');
+        let data = objectifyFormArray($form.serializeArray());
+        let id = $form.attr('id');
+        if(id){
+            contactList.update(id, data);
+        } else {
+            contactList.add(data);
+        }
+        loadListView();
     });
 
     // eventhandlers-card
@@ -47,7 +85,11 @@ $(function(){
     $('main').on('click', 'a.eventhandler-update-contact', function(e){
         e.stopPropagation();
         e.preventDefault();
-        $('main').html(templates.form_template);
+        let id = $(e.currentTarget).closest('article').attr('id');
+
+        contactList.get(id).then(function(data){
+            $('main').html(templates.form_template(data));
+        });
     });
 
     // Delete
@@ -56,19 +98,30 @@ $(function(){
         e.preventDefault();
         let id = $(e.currentTarget).closest('article').attr('id');
         contactList.delete(id);
-        $('main').html(templates.list_view);
+
+        loadListView();
     });
 
+    // Tags
+    $("main").on('click', 'a.eventhandler-filter-list', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        let tag = $(e.target).text().replace('#', '');
 
+        contactList.list().then(function(data){
+            let filteredContacts = data.filter(function(contact){
+                return contact.tags.split(/,\s?/).includes(tag);
+            });
 
-
-    // load
-    contactList.list().then(function(data){
-        let context = {
-            "hasItems": Object.keys(data).length > 0,
-            "items": data
-        };
-
-        $('main').html(templates.list_view(context));
+            let context = {
+                "hasItems": Object.keys(filteredContacts).length > 0,
+                "items": filteredContacts
+            };
+    
+            $('main').html(templates.main_view(context));
+        });
     });
+
+    // start
+    loadListView();
 });
